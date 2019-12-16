4de63a5b2e95b7c23a87a6d1d27bb68ee92b201b
minqike <minqike@foxmail.com>
2019-12-16 15:02:21 +0800

    activemq 拆分生产者和消费者

--- springboot-activemq/consumer01/src/main/java/com/min/springboot/activemq/service/TopicService.java
+++ springboot-activemq/consumer01/src/main/java/com/min/springboot/activemq/service/TopicService.java
@@ -9,13 +9,17 @@ import java.util.Date;
 @Component
 public class TopicService {
 
-    @JmsListener(destination = "ActiveMQTopic",containerFactory = "jmsTopicListenerContainerFactory")
+    @JmsListener(destination = "ActiveMQTopic",
+            containerFactory = "jmsTopicListenerContainerFactory")
     public void handleMessage1(Person person) {
-        System.out.println((new Date()).getTime() +"-Person成功接受person1=" + person.toString());
+        System.out.println((new Date()).getTime()
+                + "-Person成功接受person1=" + person.toString());
     }
 
-    @JmsListener(destination = "ActiveMQTopic",containerFactory = "jmsTopicListenerContainerFactory")
+    @JmsListener(destination = "ActiveMQTopic",
+            containerFactory = "jmsTopicListenerContainerFactory")
     public void handleMessage2(Person person) {
-        System.out.println((new Date()).getTime() +"-Person成功接受person2=" + person.toString());
+        System.out.println((new Date()).getTime()
+                + "-Person成功接受person2=" + person.toString());
     }
 }
